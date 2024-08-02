d8c64cf83466996c135675c41c42a0a6f8535ef5
tommyy1708 <hzhan081@fiu.edu>
2024-08-02 12:43:12 -0400

    UI adjusted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
-// import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* <ConfigProvider
-      theme={{
-        token: {
-
-          // Modify the token here to changed the theme of the whole app
-        },
-      }}
-    > */}
       <ThemeProvider>
           <App />
       </ThemeProvider>
-    {/* </ConfigProvider> */}
   </React.StrictMode>
 );
